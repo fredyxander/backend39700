a1c291baf4c2fc2201e55fa0c7f43e927916574a
fredyxander <freddy5210@gmail.com>
2023-05-08 18:24:52 -0500

    after desafio refatorizacion capas

---
+++ clase_27/After-proyecto con capas/base-Desafio-capas/src/config/options.js
@@ -0,0 +1,9 @@
+export const options = {
+    fileSystem:{
+        usersFileName: 'users.json',
+        productsFileName: 'products.json',
+    },
+    mongoDB:{
+        url:"Agrega tu base de datos"
+    }
+};
